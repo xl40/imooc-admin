138c217121b6f54479ad2ac9cda07cd4d85ba12c
xuel <butter202209@163.com>
2022-10-08 19:49:38 +0800

    feat: 引入依赖 element-plus，新增 login 布局和样式。

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <div id="nav"></div>
   <router-view/>
 </template>
-
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
