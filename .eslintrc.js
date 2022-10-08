b47b8fd5a1399e33476b40b15e2a4477f3c6e832
xuel <butter202209@163.com>
2022-10-08 13:00:40 +0800

    fix eslint & update

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,5 +31,17 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': "off"
-  }
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+
 }
