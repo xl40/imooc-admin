ad500e9602a7a1e2df521fab9d1e888b738fad26
xuel <butter202209@163.com>
2022-10-08 19:27:50 +0800

    fix jest config 更新jest配置

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,24 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
-  // moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-  // transform: {
-  //   '^.+\\.vue$': 'vue-jest',
-  //   '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-  //   '^.+\\.jsx?$': 'babel-jest'
-  // },
-  // transformIgnorePatterns: [
-  //   '/node_modules/'
-  // ],
-  // moduleNameMapper: {
-  //   '^@/(.*)$': '<rootDir>/src/$1'
-  // },
-  // snapshotSerializers: [
-  //   'jest-serializer-vue'
-  // ],
-  // testMatch: [
-  //   '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  // ],
-  // testURL: 'http://localhost/',
-  // watchPlugins: [
-  //   'jest-watch-typeahead/filename',
-  //   'jest-watch-typeahead/testname'
-  // ]
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)',
+    '**/tests/**/*.spec.(js|jsx|ts|tsx)',
+    '!**/__fixtures__/**',
+    '**/__tests__/**/*.js'
+  ],
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ]
 }
